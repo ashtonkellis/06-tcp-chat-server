5056e103f336d70a90484d977bb4f5d08194747b
Ashton K Ellis <ashtonkellis@gmail.com>
2018-06-21 22:46:24 -0700

    @dm working

--- src/lib/chatroom.js
+++ src/lib/chatroom.js
@@ -5,12 +5,11 @@ const net = require('net');
 const logger = require('./logger');
 const Client = require('../model/client');
 
-
 const PORT = process.env.PORT || 3000;
 
 const server = net.createServer();
 const event = new EventEmitter();
-const socketPool = {};
+const clientPool = {};
 
 const parseData = (buffer) => {
   const text = buffer.toString().trim();
@@ -32,29 +31,44 @@ const dispatchAction = (client, buffer) => {
 // these are all the event listeners
 event.on('@all', (data, client) => {
   logger.log(logger.INFO, data);
-  Object.keys(socketPool).forEach((clientIdKey) => {
-    const targetedClient = socketPool[clientIdKey];
+  data.message = data.message.join(' ');
+  Object.keys(clientPool).forEach((clientIdKey) => {
+    const targetedClient = clientPool[clientIdKey];
     targetedClient.socket.write(`<${client.nickname}>: ${data.message}\n`);
+  });
+});
+
+event.on('@dm', (data, client) => {
+  logger.log(logger.INFO, data);
+
+  const targetNickname = data.message.shift();
+  data.message = data.message.join(' ');
+
+
+  Object.keys(clientPool).forEach((clientIdKey) => {
+    const targetedClient = clientPool[clientIdKey];
+    if (targetedClient.nickname[0] === targetNickname) {
+      targetedClient.socket.write(`<${client.nickname}>: ${data.message}\n`);
+    }
   });
 });
 
 event.on('@nickname', (data, client) => {
   logger.log(logger.INFO, data);
-  socketPool[client._id].nickname = data.message;
+  clientPool[client._id].nickname = data.message;
   client.socket.write(`You have changed your client name to ${data.message}\n`);
 });
 
 event.on('@list', (data, client) => {
   logger.log(logger.INFO, data);
-  Object.keys(socketPool).forEach((clientIdKey) => {
-    client.socket.write(`${socketPool[clientIdKey].nickname}\n`);
+  Object.keys(clientPool).forEach((clientIdKey) => {
+    client.socket.write(`${clientPool[clientIdKey].nickname}\n`);
   });
 });
 
 event.on('@quit', (data, client) => {
   logger.log(logger.INFO, data);
-  // remove from socketPool
-  delete socketPool[client._id];
+  delete clientPool[client._id];
   client.socket.destroy();
 });
 
@@ -62,33 +76,20 @@ server.on('connection', (socket) => {
   const client = new Client(socket);
 
   socket.write(`Welcome to the chatroom, ${client.nickname}!\n`);
-  // keep a record of that client in our socketPool by making a 
-  // new key value pair that looks like this:
-  // { 'dafsaed922919101: { 
-  //   _id: dafsaed922919101,
-  //   nickname: Client no. dafsaed922919101,
-  //   socket: really big object
-  // }}
-  socketPool[client._id] = client;
+  clientPool[client._id] = client;
   logger.log(logger.INFO, `A new client ${client.nickname} has entered the chatroom!`);
 
   socket.on('data', (buffer) => {
     dispatchAction(client, buffer);
-  }); 
-});
+  });
 
-server.on('error', (err) => {
-  if (err.code === 'EADDRINUSE') {
-    console.log('Address in use, retrying...');
-    setTimeout(() => {
-      server.close();
-      server.listen(PORT, () => {
-        logger.log(logger.INFO, `Server up on PORT: ${PORT}`);
-      });
-    }, 1000);
-  } else {
-    console.log(err);
-  }
+  socket.on('close', () => {
+    logger.log(logger.INFO, 'Client disconnected.');
+  });
+
+  socket.on('error', (err) => {
+    logger.log(logger.INFO, err);
+  });
 });
 
 server.listen(PORT, () => {
